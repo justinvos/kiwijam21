ce7df1a0af2c8825554c43ac6d0b1ead75b1c54d
Justin Vos <justinvosnz@gmail.com>
2021-07-25 15:16:28 +1200

    Set Virtual camera priortiy

--- Assets/Scripts/ControllerScript.cs
+++ Assets/Scripts/ControllerScript.cs
@@ -11,7 +11,7 @@ public class ControllerScript : MonoBehaviour {
   public GameObject droidAdaptor;
   public GameObject semiAdaptor;
   
-  public GameObject pinsVirtualCamera;
+  public CinemachineVirtualCamera pinsVirtualCamera;
 
   public TextMeshProUGUI amperageText;
 
@@ -53,7 +53,7 @@ public class ControllerScript : MonoBehaviour {
     float startX = -3.62f;
     float incrementX = 1.46f;
     Debug.Log("HandleButtonClick:" + index);
-    pinsVirtualCamera.transform.position = new Vector3(startX + (incrementX * index), pinsVirtualCamera.transform.position.y, pinsVirtualCamera.transform.position.z);
-
+    pinsVirtualCamera.gameObject.transform.position = new Vector3(startX + (incrementX * index), pinsVirtualCamera.gameObject.transform.position.y, pinsVirtualCamera.transform.position.z);
+    pinsVirtualCamera.Priority = 22;
   }
 }
